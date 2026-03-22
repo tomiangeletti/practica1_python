4b395bc261a9718edf6b917d316fc9e3af409c5a
Eleonora Borgoglio <tomilobolaplata@gmail.com>
2026-03-21 23:18:29 -0300

    Funcionalidad: que no se repitan palabras

--- questions.py
+++ questions.py
@@ -1,58 +1,94 @@
 import random
 import string
+
 categories = {
     "programacion": ["python", "programa", "variable", "funcion", "bucle"],
     "datos": ["cadena", "entero", "lista"]
 }
-guessed = []
-attempts = 6
+
+
 score = 0
 valid_chars = string.ascii_letters
+continue_playing = True
+index = 0
+
+     
 print("¡Bienvenido al Ahorcado!")
 print()
 # Mostrar categorias
+
 print("Categorias: ")
 for category in categories:
         print("-", category)
 category_choiced = input("Elegi una categoria (escribir nombre de la categoria):")
 # Validar que la categoria sea correcta
+
 while category_choiced not in categories:
         category_choiced = input("Categoria invalida. Por favor elija de nuevo: ")
-word = random.choice(categories[category_choiced])
+
+# Crear lista con las palabras de la categoria elegida (en orden aleatorio).
+
+words_category_choiced = categories[category_choiced]
+words_random_order = random.sample(words_category_choiced, len(words_category_choiced))
+
 # Bucle principal
-while attempts > 0:
-# Mostrar progreso: letras adivinadas y guiones para las que faltan
-    progress = ""
-    for letter in word:
-        if letter in guessed:
-            progress += letter + " "
+
+while index < len(words_category_choiced):
+    guessed = []
+    attempts = 6
+    word = words_random_order[index]
+    print("prueba 1")
+    while attempts > 0 and index < len(words_category_choiced):
+    # Mostrar progreso: letras adivinadas y guiones para las que faltan
+
+        progress = ""
+        for letter in word:
+            if letter in guessed:
+                progress += letter + " "
+            else:
+                progress += "_ "
+        print(progress)
+    # Verificar si el jugador ya adivinó la palabra completa
+
+        if "_" not in progress:
+            print("¡Ganaste!")
+            score = score + 6
+            print(f"Felicitaciones! Tu puntaje fue de {score} puntos.")
+            print()
+            print()
+            choice = input("Ronda finalizada! Quieres seguir jugando? (s/n): ")
+            if choice == "s":
+                index += 1
+                guessed = []
+                attempts = 6
+                word = words_random_order[index]
+            elif choice == "n":
+                #continue_playing = False
+                break
         else:
-            progress += "_ "
-    print(progress)
-# Verificar si el jugador ya adivinó la palabra completa
-    if "_" not in progress:
-        print("¡Ganaste!")
-        score = score + 6
-        print(f"Felicitaciones! Tu puntaje fue de {score} puntos.")
-        break
-    print(f"Intentos restantes: {attempts}")
-    print(f"Letras usadas: {', '.join(guessed)}")
-    letter = input("Ingresá una letra: ")
-    if letter in valid_chars and len(letter) == 1:
-        if letter in guessed:
-            print("Ya usaste esa letra.")
-        elif letter in word:
-            guessed.append(letter)
-            print("¡Bien! Esa letra está en la palabra.")
-        else:
-            guessed.append(letter)
-            attempts -= 1
-            print("Esa letra no está en la palabra.")
-            score = score -1
-    else:
-        print("Entrada no valida.")
-    print()
-else:
+            print(f"Intentos restantes: {attempts}")
+            print(f"Letras usadas: {', '.join(guessed)}")
+            letter = input("Ingresá una letra: ")
+            if letter in valid_chars and len(letter) == 1:
+                if letter in guessed:
+                    print("Ya usaste esa letra.")
+                elif letter in word:
+                    guessed.append(letter)
+                    print("¡Bien! Esa letra está en la palabra.")
+                else:
+                    guessed.append(letter)
+                    attempts -= 1
+                    print("Esa letra no está en la palabra.")
+                    score = score -1
+            else:
+                print("Entrada no valida.")
+            print()
     print(f"¡Perdiste! La palabra era: {word}")
     score = 0
     print(f"Puntaje: {score}")
+    choice = input("Ronda finalizada! Quieres seguir jugando? (s/n): ")
+    if choice == "s":
+        index += 1
+    elif choice == "n":
+        #continue_playing = False
+        break
